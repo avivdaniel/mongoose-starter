34d0e4105918fda367bf61549eaf8ba20239997b
avivdaniel <avivdaniel@gmail.com>
2020-05-19 19:09:04 +0300

    add Post Schema and PUT post ROUTE

--- src/server.js
+++ src/server.js
@@ -2,7 +2,46 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 const port = 3000;
+
+mongoose.connect('mongodb://localhost:27017/social',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+const db = mongoose.connection;
+db.on('error', err => console.log(err));
+db.once('open', () => console.log('connected to MongoDB'));
+
+const User = new mongoose.model('User', {
+    name: String,
+    username: {
+        type: String,
+        required: true
+    },
+    email: String,
+    created: {
+        type: Date,
+        default: new Date()
+    }
+});
+
+const Post = new mongoose.model('Post', {
+    content: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: new Date()
+    },
+    userId: {
+        type: String,
+        required: true
+    }
+});
 
 app.use(cors());
 
@@ -14,22 +53,71 @@ app.use(bodyParser.json());
 
 
 app.put('/user', (req, res) => {
-
+    const user = new User(req.body);
+    // const User = new User({
+    //     username: 'Aviv'
+    // })
+    user.save() //async
+        .then(user => res.status(201).json(user))
+        .catch(err => res.status(400).json(err));
 });
 
 app.get('/user', (req, res) => {
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json(err));
 });
 
 app.get('/user/:id', (req, res) => {
-
+    // User.findOne({
+    //     _id: req.params.id
+    // })
+    //     .then(user => res.json(user))
+    //     .catch(err => res.json(err));
+    User.findById(req.params.id)
+        .then(user => {
+            if (!user) {
+                res.sendStatus(404);
+                return;
+            }
+            res.json(user);
+        })
+        .catch(err => res.json(err));
 });
 
 app.post('/user/:id', (req, res) => {
-
+    User.findById(req.params.id)
+        .then(user => {
+            if (!user) {
+                res.sendStatus(404);
+                return;
+            }
+            user.update(req.body)
+                .then(() => res.sendStatus(200))
+                .catch(err => res.status(400).json(err));
+        })
+        .catch(err => res.status(400).json(err));
 });
 
 app.delete('/user/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if (!user) {
+                res.sendStatus(404);
+                return;
+            }
+            user.remove()
+                .then(() => res.sendStatus(204))
+                .catch(err => res.status(400).json(err));
+        })
+        .catch(err => res.status(400).json(err));
+});
 
+app.put('/post', (req, res) => {
+    const post = new Post(req.body);
+    post.save() //async
+        .then(post => res.status(201).json(post))
+        .catch(err => res.status(400).json(err));
 });
 
 
